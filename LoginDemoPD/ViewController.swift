4b6e46a9ef920a1cb0c174ef888bc8e9a9d44f7f
Daniel Arturo <danygox@hotmail.com>
2019-04-02 09:58:44 -0600

    Initial Commit

---
+++ LoginDemoPD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginDemoPD
+//
+//  Created by Hugo Juárez on 02/04/19.
+//  Copyright © 2019 Hugo Juárez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
